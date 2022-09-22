5c4df2ff75ecc10d0bbdd175333acb01c6e64e3b
HarshaanNiles010 <harshaanniles010@gmail.com>
2022-09-22 14:33:00 -0600

    Created basic search

--- app/src/main/java/com/example/cryptotracker/MainActivity.java
+++ app/src/main/java/com/example/cryptotracker/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.cryptotracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    Button searchButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.CryptoToken);
+        searchButton = (Button) findViewById(R.id.search);
     }
 }
